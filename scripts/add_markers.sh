1a0f3b544a7bdf97e067032dcdd321582e91332d
amrtariq <amrtariq@gmail.com>
2022-02-26 05:58:40 +0200

    made paths relative

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 currentPath=$(pwd)
 echo $currentPath
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$currentPath/../../src/worlds/myworld.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find worlds)s/myworld.world " &
 sleep 10
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$currentPath/../../src/map/mymap.yaml " &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find map)/mymap.yaml " &
 sleep 7
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 7
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 currentPath=$(pwd)
 echo $currentPath
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$currentPath/../../src/worlds/myworld.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find worlds)/myworld.world " &
 sleep 10
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 3
